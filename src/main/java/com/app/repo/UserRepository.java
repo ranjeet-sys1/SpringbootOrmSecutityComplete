76f86d77d69a5bb7b1a059d67710fe5d987f5ff6
ranjeet.suman <ranjeet.suman@technoidentity.com>
2022-02-06 02:26:15 +0530

    added

--- src/main/java/com/app/repo/UserRepository.java
+++ src/main/java/com/app/repo/UserRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.model.User;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+	@Query(" from User where email=:userEmail")
 	Optional<User> findByEmail(String userEmail);
 }
